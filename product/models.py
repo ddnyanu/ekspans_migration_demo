ee1fa633eb6075bb2732d8d94823dc5694104183
ddnyanu <dhotrednayanu@gmail.com>
2026-04-29 15:37:17 +0530

    type field name changed

--- product/models.py
+++ product/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
-    type= models.CharField(max_length=255,blank=True,null=True)
+    category= models.CharField(max_length=255,blank=True,null=True)
     
     class Meta:
         db_table = "product_data"
